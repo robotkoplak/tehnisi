05679b6ec8f739d6ad5ca072af394323a96f1cca
robotkoplak <noc@sblnetwork.co.id>
2024-10-19 07:44:23 +0700

    fix

--- views/home.php
+++ views/home.php
@@ -1,10 +1,10 @@
 <?php
 $pageTitle = 'Selamat Datang di Aplikasi Tiket Teknisi';
-require_once 'includes/header.php';
+require_once '../includes/header.php';
 ?>
 
 <h1>Selamat Datang di Aplikasi Tiket Teknisi</h1>
 <p>Silakan login untuk melanjutkan.</p>
 <a href="login.php" class="btn">Login</a>
 
-<?php require_once 'includes/footer.php'; ?>
+<?php require_once '../includes/footer.php'; ?>
